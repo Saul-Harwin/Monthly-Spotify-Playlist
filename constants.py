054c952ffe5eba0fe12360e9d44d72a468deb262
Saul-Harwin <saulharwin@gmail.com>
2024-08-22 21:20:39 +0100

    Added needed changed to allow for running from user crontab

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 SPOTIPY_CLIENT_ID="7633f7aece7d4fd882c204c454719535"
 SPOTIPY_CLIENT_SECRET="c39372e7cc284908b29143c9e2eabad2"
-SPOTIPY_REDIRECT_URI="http://127.0.0.1:8050"
+SPOTIPY_REDIRECT_URI="http://127.0.0.1:5000"
 SCOPE = "user-top-read, playlist-modify-private, playlist-modify-public"
+CACHE_PATH = "/home/saul/Documents/Projects/Monthly-Spotify-Playlist/.cache"
 MONTHS = {
     "1": "January",
     "2": "Febuary",
--- run.py
+++ run.py
@@ -2,11 +2,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import constants as const
 from helpers import *
-from datetime import date
+from datetime import date, datetime
 
 
 def run():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=const.SPOTIPY_CLIENT_ID, client_secret=const.SPOTIPY_CLIENT_SECRET, redirect_uri=const.SPOTIPY_REDIRECT_URI, scope=const.SCOPE))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=const.SPOTIPY_CLIENT_ID, client_secret=const.SPOTIPY_CLIENT_SECRET, redirect_uri=const.SPOTIPY_REDIRECT_URI, scope=const.SCOPE, open_browser=False, cache_path=const.CACHE_PATH))
     results = sp.current_user_top_tracks(limit=25, offset=0, time_range="short_term")
     
     playlist_name = create_playlist(sp, const.MONTHS)
@@ -14,4 +14,6 @@ def run():
     add_songs_to_playlist(sp, trackIDs, playlist_name)
 
 if __name__ == "__main__":
+    print("\n")
+    print(datetime.now())
     run()
