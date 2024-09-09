a2b7df1fa48546bdd3ebe913aa18fe6ca0cdc522
Saul-Harwin <saulharwin@gmail.com>
2024-09-09 16:37:22 +0100

    .gitignore is now working

---
+++ constants.py
@@ -0,0 +1,19 @@
+SPOTIPY_CLIENT_ID="7633f7aece7d4fd882c204c454719535"
+SPOTIPY_CLIENT_SECRET="c39372e7cc284908b29143c9e2eabad2"
+SPOTIPY_REDIRECT_URI="http://127.0.0.1:5000"
+SCOPE = "user-top-read, playlist-modify-private, playlist-modify-public"
+CACHE_PATH = "/home/saul/Documents/Projects/Monthly-Spotify-Playlist/.cache"
+MONTHS = {
+    "1": "January",
+    "2": "Febuary",
+    "3": "March",
+    "4": "April",
+    "5": "May",
+    "6": "June",
+    "7": "July",
+    "8": "August",
+    "9": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+    }
