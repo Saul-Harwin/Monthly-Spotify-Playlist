207f7763a46ea1cb932724a67687e702403ad1f6
Saul-Harwin <saulharwin@gmail.com>
2024-09-09 16:32:00 +0100

    Security Update

---
+++ constants.py
@@ -0,0 +1,19 @@
+SPOTIPY_CLIENT_ID=""
+SPOTIPY_CLIENT_SECRET=""
+SPOTIPY_REDIRECT_URI="http://127.0.0.1:5000"
+SCOPE = "user-top-read, playlist-modify-private, playlist-modify-public"
+CACHE_PATH = "/home/saul/Documents/Projects/Monthly-Spotify-Playlist/.cache"
+MONTHS = {
+    "1": "January",
+    "2": "Febuary",
+    "3": "March",
+    "4": "April",
+    "5": "May",
+    "6": "June",
+    "7": "July",
+    "8": "August",
+    "9": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+    }
