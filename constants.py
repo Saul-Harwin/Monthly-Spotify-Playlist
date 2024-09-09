1a7fd1039b9a37f4613e73716e1c7510bed31ecd
Saul-Harwin <saulharwin@gmail.com>
2024-09-09 16:11:15 +0100

    Security Update

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-SPOTIPY_CLIENT_ID="7633f7aece7d4fd882c204c454719535"
-SPOTIPY_CLIENT_SECRET="c39372e7cc284908b29143c9e2eabad2"
+SPOTIPY_CLIENT_ID=""
+SPOTIPY_CLIENT_SECRET=""
 SPOTIPY_REDIRECT_URI="http://127.0.0.1:5000"
 SCOPE = "user-top-read, playlist-modify-private, playlist-modify-public"
 CACHE_PATH = "/home/saul/Documents/Projects/Monthly-Spotify-Playlist/.cache"
